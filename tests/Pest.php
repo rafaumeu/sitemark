b0ce4186b985e35ff4463c70750fbd25ec25e77e
Rafael Dias Zendron <mmmarckos@gmail.com>
2026-05-07 20:41:22 -0300

    feat: add Pest PHP tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +14,7 @@ declare(strict_types = 1);
 */
 
 pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
 
 /*
@@ -23,8 +23,8 @@ pest()->extend(Tests\TestCase::class)
 |--------------------------------------------------------------------------
 |
 | When you're writing tests, you often need to check that values meet certain conditions. The
-| "expect()" function gives you access to a set of "expectations" methods that you can use
-| to assert different things. Of course, you may extend the Expectation API at any time.
+| "expect()" function gives you access to a set of "expectations" methods that you can use to
+| assert different things. Of course, you may extend the Expectation API at any time.
 |
 */
 
@@ -37,13 +37,8 @@ expect()->extend('toBeOne', function () {
 | Functions
 |--------------------------------------------------------------------------
 |
-| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
+| While Pest is very powerful out of the box, you may have some testing code specific to your
 | project that you don't want to repeat in every file. Here you can also expose helpers as
 | global functions to help you to reduce the number of lines of code in your test files.
 |
 */
-
-function something()
-{
-    // ..
-}
